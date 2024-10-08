8fca1fbe504578834b52ea956f90cd92a56c6a04
Alec <alecpierce19@gmail.com>
2024-10-08 17:10:49 -0500

    fixed tests

--- app/src/main/java/com/solutions/EvenFibonacciTests.java
+++ app/src/main/java/com/solutions/EvenFibonacciTests.java
@@ -7,27 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 public class EvenFibonacciTests {
 
     @Test
-    public void testSumOfEvenFibonacciBelow10() {
+    public void testSumOfEvenFibonacciReturnsNumber() {
         assertInstanceOf(Integer.class, EvenFibonacci.findSumOfEvenFibonacci(10));
     }
 
     @Test
-    public void testSumOfEvenFibonacciBelow20() {
-        assertEquals(10, EvenFibonacci.findSumOfEvenFibonacci(20));
+    public void testSumOfEvenFibonacci10() { assertEquals(10, EvenFibonacci.findSumOfEvenFibonacci(10)); }
+
+    @Test
+    public void testSumOfEvenFibonacci34() {
+        assertEquals(44, EvenFibonacci.findSumOfEvenFibonacci(34));
     }
 
     @Test
-    public void testSumOfEvenFibonacciBelow100() {
-        assertEquals(44, EvenFibonacci.findSumOfEvenFibonacci(100));
+    public void testSumOfEvenFibonacci60() {
+        assertEquals(44, EvenFibonacci.findSumOfEvenFibonacci(60));
     }
 
     @Test
-    public void testSumOfEvenFibonacciBelow1000() {
+    public void testSumOfEvenFibonacci1000() {
         assertEquals(798, EvenFibonacci.findSumOfEvenFibonacci(1000));
     }
 
     @Test
-    public void testSumOfEvenFibonacciBelow10000() {
-        assertEquals(3382, EvenFibonacci.findSumOfEvenFibonacci(10000));
+    public void testSumOfEvenFibonacci100000() {
+        assertEquals(60696, EvenFibonacci.findSumOfEvenFibonacci(100000));
     }
+
+    @Test
+    public void testSumOfEvenFibonacci4000000() {
+        assertEquals(4613732, EvenFibonacci.findSumOfEvenFibonacci(4000000));
+    }
+
 }
